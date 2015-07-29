39c08fcbf60fe98f05949108a6c0a4f56358d9ce
unknown <administrator@CALPMPS04.orsypgroup.com>
2015-07-29 14:58:04 -0400

    New Classes with main added

---
+++ core/src/com/orsyp/tools/ps/CopyOfTest.java
@@ -0,0 +1,28 @@
+package com.orsyp.tools.ps;
+
+
+public class CopyOfTest {
+
+	public static void main(String[] args) throws Exception {
+
+		String fileName = args[0];
+		
+		
+		Connector conn = new Connector(fileName,true,"",true,"",true,"");
+	
+		
+		
+		
+		
+		for(String uprKey:conn.getConnectionList().get(0).getUprocHashMap_from_outside().keySet())
+		{
+			
+
+				conn.getConnectionList().get(0).setMemorizationOnUproc(uprKey);
+				
+		}
+	}	
+		
+	
+
+}
